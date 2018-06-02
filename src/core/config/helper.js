3e2471ec94f2b5abf10f15fbaef7eb327bd88b8b
Chris Rocco <chris.rocco7@gmail.com>
2018-06-01 21:20:04 -0500

    acidentally compiled to /src. this fix removes .js files from src/

--- src/core/config/helper.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var lodash = require("lodash");
-exports.getConfigHelper = function (config) { return function (key) {
-    var V = lodash.get(config, key);
-    if (V === null)
-        throw new Error("Invalid config value accessed: " + key);
-    return V;
-}; };
--- src/core/database/_entities.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Pipe_1 = require("../../pipes/Pipe");
-exports.ENTITIES = [
-    Pipe_1.Pipe
-];

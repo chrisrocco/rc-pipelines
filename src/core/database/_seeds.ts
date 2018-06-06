b6b6ee3035d81c3edf911f2e8b1eed506004fa01
Chris Rocco <chris.rocco7@gmail.com>
2018-06-06 13:27:16 -0500

    ?

--- src/core/database/_seeds.ts
+++ src/core/database/_seeds.ts
@@ -12,8 +12,12 @@ export const DATABASE = {
             id: 2,
             event: 'user-groups.29y8.user-joined',
             digest: 'data-extraction.423.add-user'
+        },
+        {
+            id: 3,
+            event: 'pub-repos.116a15fc-2aa6-4fec-b6b2-2e6e5f6b9189.pubs-added',
+            digest: 'data-extraction.23452r.start-extraction'
         }
     ]
 
 }
-
